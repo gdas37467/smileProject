27425def481279cd56930a04c3da23bf4b00a67a
gdas <gdas37467@gmail.com>
2024-05-17 08:51:57 +0530

    done

--- Blood-bank-backend/smile/settings.py
+++ Blood-bank-backend/smile/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_crontab',
+   
 ]
 
 MIDDLEWARE = [
@@ -256,3 +257,6 @@ CRONJOBS = [
     ('0 0 * * *', 'smile.cronjob.updateUnits', '>> ' + os.path.join(BASE_DIR,'log/debug7.log' + ' 2>&1 ')),
     # Add more cron jobs as needed
 ]
+
+
+
